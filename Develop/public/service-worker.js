8e9ddd69ab98761a096c8afb9fc58ac3ecc71770
Natalie Rodriguez <natalierodri96@gmail.com>
2020-11-25 18:39:41 -0600

    service worker

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -6,7 +6,7 @@ const FILES_TO_CACHE = [
   "/webpack.config.js",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
-  "/db.js",
+  "/offline.js",
 ];
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
